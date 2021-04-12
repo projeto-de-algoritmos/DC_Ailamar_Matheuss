64193a4b0e4d0b12babf339d8b95c89193db8acc
Matheus Amaral <mam.macacod@gmail.com>
2021-04-12 18:47:51 -0300

    add a button who gives the median

--- frontend/main/script.js
+++ frontend/main/script.js
@@ -1,56 +1,52 @@
-(function main () {
-    let teste = [20, 80, 1, 4, 90, 
+let elementos = document.getElementById("main");
+
+let botao = document.getElementById("botao");
+
+botao.addEventListener("click", main);
+
+
+function main () {
+    let teste = [90, 80, 1, 4,  20, 
                 10, 100, 2, 50, 40,
+                99, 300, 8, 5, 400,
                 60, 30, 120, 3, 5]
     
-    let media = mediana(teste);
-    
+    let media = medianaMedianas(teste);
     console.log(media);
-
-})()
-
-function medianaMedianas (vetor) {
-    // insertion sort
-    for (let i = 1, end = vetor.length; i < end; i++) {
-        for (let j = i; j > 0; j--) {
-            comparaTroca(vetor[j], vetor[j-1]);
-        }
-    }
-
+    elementos.innerHTML = `<p>vetor: ${teste}</p>`;
+    
+    elementos.innerHTML += `<p>media: ${media}</p>`;
 }
 
 function medianaV (vetor) {
-
-    // ordena mediana em 9 iteracoes
-
-    comparaTroca(vetor[0], vetor[4]);
-    comparaTroca(vetor[1], vetor[4]);
-    comparaTroca(vetor[2], vetor[4]);
-    comparaTroca(vetor[3], vetor[4]);
-
-    comparaTroca(vetor[0], vetor[3]);
-    comparaTroca(vetor[1], vetor[3]);
-    comparaTroca(vetor[2], vetor[3]);
-    
-    comparaTroca(vetor[0], vetor[2]);
-    comparaTroca(vetor[1], vetor[2]);
+    for (let i = 1, end = vetor.length; i < end; i++) {
+        for (let j = i; j > 0; j--) {
+            if (vetor[j] < vetor[j-1]) [vetor[j], vetor[j-1]] = [vetor[j-1], vetor[j]];
+        }
+    }
     // mediana ordenada
-    
+    console.log(vetor);
+
     return vetor[2];
 }
 
-function comparaTroca (a, b) {
-    if (b < a) [a, b] = [b, a];
-}
-
-function mediana (teste) {
+function medianaMedianas (amostral) {
     let medianas = [];
-    for (let i = 0, end = teste.length; i < end; i+=5) {
-        medianas.push(medianaV([teste[i], teste[i+1], teste[i+2], teste[i+3], teste[i+4]]));
-    }
-    return medianaMedianas(medianas);
-}
+    let vetor = amostral;
+    let end = vetor.length;
+    do{
+        for (let i = 0; i < end; i+=5) {
+            medianas.push(medianaV([vetor[i], vetor[i+1], vetor[i+2], vetor[i+3], vetor[i+4]]));
+        }
+        vetor = medianas;
+        end = vetor.length;
+        medianas = [];
+    }while(vetor.length-1 > 4);
 
+    medianaV(vetor);
+
+    return vetor[Number.parseInt((end-1)/2)];
+}
 
 /*
 0  1   2   3  4
